41f01efea1e8797727517f4443b111e7e99500cb
Nate Lee <nleepercussion@gmail.com>
2021-08-02 14:30:21 -0400

    Finished tutorial

---
+++ cypress/integration/init.spec.js
@@ -0,0 +1,53 @@
+describe('Cypress', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('focuses on the input', () => {
+        cy.focused().should('have.class', 'form-control')
+    })
+
+    it('accepts input', () => {
+        const input = "Learn about Cypress"
+        cy.get('.form-control')
+          .type(input)
+          .should('have.value', input)
+    })
+
+    it('displays a list of todos', () => {
+        cy.get('li')
+          .should('have.length', 2)
+    })
+
+    it('adds a new todo', () => {
+        const input = "Learn about cypress"
+        cy.get('.form-control')
+          .type(input)
+          .type('{enter}')
+          .get('li')
+          .should('have.length', 3)
+    })
+
+    it('deletes a todo', () => {
+        cy.get('li')
+          .first()
+          .find('.btn-danger')
+          .click()
+          .get('li')
+          .should('have.length', 1)
+    })
+
+    it('deletes all todo', () => {
+        cy.get('li')
+        .first()
+        .find('.btn-danger')
+        .click()
+        .get('li')
+        .first()
+        .find('.btn-danger')
+        .click()
+        .get('.no-task')
+        .should('have.text', 'No task!')
+
+    })
+})
